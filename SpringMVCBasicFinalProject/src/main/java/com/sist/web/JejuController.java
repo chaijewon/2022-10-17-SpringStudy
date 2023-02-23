668a70d06748d1a9b5d15f49da4415f3b643be43
user <user@DESKTOP-COJ9858>
2023-02-23 16:47:16 +0900

    1111

--- SpringMVCBasicFinalProject/src/main/java/com/sist/web/JejuController.java
+++ SpringMVCBasicFinalProject/src/main/java/com/sist/web/JejuController.java
@@ -3,9 +3,17 @@ package com.sist.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.*;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import com.sist.dao.*;
 import com.sist.vo.*;
 @Controller
@@ -16,7 +24,7 @@ public class JejuController {
    
    @GetMapping("jeju/list.do")
    //jeju/list.do?page=1&no=10
-   public String jeju_list(String page,Model model)
+   public String jeju_list(String page,Model model,HttpServletRequest request)
    {
 	   if(page==null)
 		   page="1";
@@ -53,7 +61,44 @@ public class JejuController {
 	   model.addAttribute("startPage", startPage);
 	   model.addAttribute("endPage", endPage);
 	   model.addAttribute("list", list);
+	   
+	   List<JejuLocationVO> cList=new ArrayList<JejuLocationVO>();
+	   // 쿠키 관련 
+	   Cookie[] cookies=request.getCookies();
+	   if(cookies!=null)
+	   {
+		   for(int i=cookies.length-1;i>=0;i--)
+		   {
+			   if(cookies[i].getName().startsWith("jeju"))
+			   {
+				   String no=cookies[i].getValue();
+				   JejuLocationVO vo=dao.jejuDetailData(Integer.parseInt(no));
+				   cList.add(vo);
+			   }
+		   }
+	   }
+	   model.addAttribute("cList", cList);
 	   return "jeju/list";
+   }
+   //jeju/detail_before.do?no=${vo.no }
+   /*
+    *  String no=request.getParameter("no")
+    *  Integer.parseInt(no)
+    *  
+    *  jeju_detail_before(no)
+    *  jeju_detail_before(Integer.parseInt(no))
+    */
+   @GetMapping("jeju/detail_before.do")
+   public String jeju_detail_before(String no,HttpServletResponse response,RedirectAttributes ra)
+   {
+	   // String no=10
+	   Cookie cookie=new Cookie("jeju"+no, no);
+	   cookie.setPath("/");
+	   cookie.setMaxAge(60*60*24);
+	   // 브라우저로 전송 
+	   response.addCookie(cookie);
+	   ra.addAttribute("no", no);
+	   return "redirect:detail.do";
    }
    //jeju/detail.do?no=${vo.no }
    @GetMapping("jeju/detail.do")
