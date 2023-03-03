a29d36bbe9b129fa20314a5a15664d3212c70d54
user <user@DESKTOP-COJ9858>
2023-03-03 16:14:40 +0900

    1111

--- SpringTilesProject2/src/main/java/com/sist/web/FoodRestContoller.java
+++ SpringTilesProject2/src/main/java/com/sist/web/FoodRestContoller.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+
 import java.util.*;
 import com.sist.dao.*;
 import com.sist.vo.*;
@@ -80,6 +83,86 @@ public class FoodRestContoller {
 	   obj.put("price", vo.getPrice());
 	   obj.put("type", vo.getType());
 	   obj.put("menu", vo.getMenu());
+	   return obj.toJSONString();
+   }
+   @GetMapping(value="food/category_vue.do",produces = "text/plain;charset=UTF-8")
+   public String category_vue(int no)
+   {
+	   Map map=new HashMap();
+	   map.put("no", no);
+	   List<CategoryVO> list=dao.categoryVueData(map);
+	   
+	   // JavaScript=> 인식 [{}]
+	   JSONArray arr=new JSONArray(); //[] JSONObject {} ==> [{},{},{},{}...]
+	   for(CategoryVO vo:list)
+	   {
+		   JSONObject obj=new JSONObject();
+		   obj.put("cno", vo.getCno());
+		   obj.put("title", vo.getTitle());
+		   obj.put("poster", vo.getPoster()); //{"cno":1,"title":...,"poster":....}
+		   arr.add(obj);
+	   }
+	   return arr.toJSONString();
+   }
+   
+   @GetMapping(value="food/food_list_vue.do",produces = "text/plain;charset=UTF-8")
+   public String food_list_vue(int no)
+   {
+	   List<FoodVO> list=dao.foodListData(no);
+	   CategoryVO cvo=dao.categoryInfoData(no);
+	   JSONArray arr=new JSONArray();
+	   // fno,cno,name,tel,address,type,poster,score
+	   int i=0;
+	   for(FoodVO vo:list)
+	   {
+		   JSONObject obj=new JSONObject();
+		   obj.put("fno",vo.getFno());
+		   obj.put("name", vo.getName());
+		   obj.put("tel", vo.getTel());
+		   String address=vo.getAddress();
+		   address=address.substring(0,address.lastIndexOf("지"));
+		   obj.put("address", address.trim());
+		   obj.put("type", vo.getType());
+		   obj.put("score", vo.getScore());
+		   String poster=vo.getPoster();
+		   poster=poster.substring(0,poster.indexOf("^"));
+		   poster=poster.replace("#", "&");
+		   obj.put("poster", poster);
+		   if(i==0)
+		   {
+			   obj.put("title", cvo.getTitle());
+			   obj.put("subject", cvo.getSubject());
+		   }
+		   arr.add(obj);
+		   i++;
+	   }
+	   return arr.toJSONString();
+   }
+   
+   //axios.get + axios.post = RequestMapping
+   @GetMapping(value="food/food_detail_vue.do",produces = "text/plain;charset=UTF-8")
+   // text/html , text/plain , text/xml(X) 
+   /*
+    *  private int fno,cno,good,soso,bad,count;
+    private double score;
+    private String poster,name,address,tel,type,price,parking,menu,time;
+    */
+   public String food_detail_vue(int fno)
+   {
+	   FoodVO vo=dao.foodDetailData(fno);
+	   JSONObject obj=new JSONObject();
+	   obj.put("address",vo.getAddress());
+	   obj.put("fno", vo.getFno());
+	   obj.put("score", vo.getScore());
+	   obj.put("name", vo.getName());
+	   obj.put("tel", vo.getTel());
+	   obj.put("type", vo.getType());
+	   obj.put("price", vo.getPrice());
+	   obj.put("parking", vo.getParking());
+	   obj.put("menu", vo.getMenu());
+	   obj.put("time", vo.getTime());
+	   obj.put("poster", vo.getPoster());
+	   
 	   return obj.toJSONString();
    }
    
